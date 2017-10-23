2877fa4c370e7f605d4254089e58125068e9363f
huang-qing <813293172@qq.com>
2017-10-23 13:24:16 +0800

    调整修复字体重叠

--- codingRuleView/js/codingRuleView.js
+++ codingRuleView/js/codingRuleView.js
@@ -1,18 +1,27 @@
-
+/*!
+ * codeing-rule-view v1.2.0 -  jQuery  plug
+ *
+ * Includes jquery.js
+ *
+ * Copyright © 2017-2018 huangqing
+ * Released under the MIT license
+ *
+ * Date: 2017-10-25
+ */
 
 (function ($) {
-   
+
     'use strict';
     $.fn.extend({
         codingRuleView: function (opt) {
-          
+
             var container = this,
                 htmlTemplate,
                 html = "",
                 iconType,
                 htmlCodeingRuleGroup = "",
                 htmlCodingRuleIcon = "",
-                htmlCodeingRuleGroupArray=[];
+                htmlCodeingRuleGroupArray = [];
 
             iconType = {
                 "01": "number",
@@ -24,28 +33,29 @@
             };
 
             htmlTemplate = {
-                    codeingRule:'<div class="codingRule">[codingRule-group]</div>',
-                    codeingRuleGroup:['<div class="codingRule-group codingRule-group-[codingRule-group-odd|even]">',
-                                            '<div class="codeingRule-text-container codeingRule-text-container-top">',
-                                                '<span class="codeingRule-line">',
-                                                    '<span class="codeingRule-text" >',
-                                                        '[codeingRule-text]',
-                                                    '</span>',
-                                                '</span>',
-                                            '</div>',
-                                            '<div class="codingRule-icons">',
-                                                '[codingRule-icons]',
-                                            '</div>',
-                                            '<div class="codeingRule-text-container codeingRule-text-container-bottom">',
-                                                '<span class="codeingRule-line">',
-                                                    '<span class="codeingRule-text" >',
-                                                        '[codeingRule-text]',
-                                                    '</span>',
-                                                '</span>',
-                                            '</div>',
-                                        '</div>'].join(""),
-                    codingRuleIcon:'<span class="codingRule-icon codingRule-icon-[codingRule-icon-type]"></span>'
-                };
+                codeingRule: '<div class="codingRule">[codingRule-group]</div>',
+                codeingRuleGroup: ['<div class="codingRule-group codingRule-group-[codingRule-group-odd|even]">',
+                    '<div class="codeingRule-text-container codeingRule-text-container-top">',
+                    '<span class="codeingRule-line">',
+                    '<span class="codeingRule-text" title="[codeingRule-text]" >',
+                    '[codeingRule-text]',
+                    '</span>',
+                    '</span>',
+                    '</div>',
+                    '<div class="codingRule-icons">',
+                    '[codingRule-icons]',
+                    '</div>',
+                    '<div class="codeingRule-text-container codeingRule-text-container-bottom">',
+                    '<span class="codeingRule-line">',
+                    '<span class="codeingRule-text" title="[codeingRule-text]" >',
+                    '[codeingRule-text]',
+                    '</span>',
+                    '</span>',
+                    '</div>',
+                    '</div>'
+                ].join(""),
+                codingRuleIcon: '<span class="codingRule-icon codingRule-icon-[codingRule-icon-type]"></span>'
+            };
 
             if (opt && opt instanceof Array) {
                 var item,
@@ -62,15 +72,14 @@
                     codingLength = item.length;
 
                     htmlCodeingRuleGroup = htmlTemplate.codeingRuleGroup.replace(/\[codeingRule-text\]/g, name).
-                        replace(/\[codingRule-group-odd\|even\]/g, i % 2 === 0 ? "even" : "odd");
+                    replace(/\[codingRule-group-odd\|even\]/g, i % 2 === 0 ? "even" : "odd");
 
                     htmlCodingRuleIcon = "";
 
                     if (codingLength === 0) {
                         htmlCodingRuleIcon += htmlTemplate.codingRuleIcon.replace(/\[codingRule-icon-type\]/g, type);
                         htmlCodingRuleIcon += htmlTemplate.codingRuleIcon.replace(/\[codingRule-icon-type\]/g, iconType["00"]);
-                    }
-                    else {
+                    } else {
                         for (var j = 0; j < codingLength; j++) {
                             htmlCodingRuleIcon += htmlTemplate.codingRuleIcon.replace(/\[codingRule-icon-type\]/g, type);
                         }
@@ -83,8 +92,53 @@
                 html = html.replace(/\[codingRule-group\]/g, htmlCodeingRuleGroupArray.join(""));
             }
 
-            return container.append(html);
+            container.append(html);
+
+            //设定总体及文字宽度
+            var group = container.find('.codingRule-group'),
+                width = 100,
+                current,
+                next,
+                currentWidth,
+                nextWidth,
+                currentText,
+                offset = 0,
+                filter;
+
+            for (var i = 0, len = group.length; i < len; i++) {
+                //计算每个分组的宽度
+                current = $(group[i]);
+                next = $(group[i + 1]);
+                filter = i % 2 === 0 ? 'codeingRule-text-container-bottom' : 'codeingRule-text-container-top';
+
+                currentWidth = current.width();
+                if (next.length === 0) {
+                    nextWidth = 100;
+                } else {
+                    nextWidth = next.width();
+                }
+
+                //计算总宽度
+                width += currentWidth;
+
+                currentText = current.find('.' + filter + ' .codeingRule-text');
+                //使用偏移量为基数计算文字宽度减值
+                if (i === 0) {
+                    offset = parseInt(currentText.css('margin-left'));
+                    if (isNaN(offset)) {
+                        offset = 0;
+                    } else {
+                        offset = -offset * 3;
+                    }
+
+                }
+                //计算文字显示宽度
+                currentText.width(currentWidth + nextWidth + offset);
+            }
+
+            container.find('>.codingRule').width(width);
+
+            return container;
         }
     });
 })(jQuery);
-
